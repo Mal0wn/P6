a8ae1d7c082cce61e2f18cd69be1f8155e401146
Malo <malomclt@gmail.com>
2020-09-26 14:35:01 +0200

    cleaned frontend

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,6 +1,8 @@
 const Sauce = require("../models/Sauce");
 const fs = require('fs');
 
+
+//création de la sauce 
 exports.createSauce = (req,res,next) => {
     const sauceObject = JSON.parse(req.body.thing);
     delete sauceObject._id;
@@ -13,6 +15,7 @@ exports.createSauce = (req,res,next) => {
     .catch(error=> res.status(400).json({error}));
 };
 
+// modifier la sauce
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
       {
@@ -24,6 +27,8 @@ exports.modifySauce = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   };
 
+
+// effacer la sauce
   exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => {
@@ -37,13 +42,15 @@ exports.modifySauce = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
 
-
+// Chercher une sauce
 exports.getOneSauce =(req,res,next) => {
     Sauce.findOne({ _id: req.params.id})
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({error}))
 };
 
+
+//Cherchher toute les sauces
 exports.getAllSauce = (req, res, next) => {
     Sauce.find()
     .then(sauces => res.status(200).json(sauces))
